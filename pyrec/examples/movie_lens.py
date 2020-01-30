390670a842a6711bccf3a2801d99f5f769464d67
robert <robert.cvitkovic@gmail.com>
2020-01-30 17:47:23 +0100

    update movie lens example

--- pyrec/examples/movie_lens.py
+++ pyrec/examples/movie_lens.py
@@ -15,53 +15,75 @@ RATINGS_FILE = "../../data/MovieLens/ml-latest-small/ratings.csv"
 uir_data = UIRData.from_csv(RATINGS_FILE)
 
 # create inventory for simulations
-inv = Inventory(uir_data)
-# u_inv = UniformInventory(uir_data)
+UNIFORM = True
+if UNIFORM:
+    inv = UniformInventory(uir_data)
+else:
+    inv = Inventory(uir_data)
 
 # fit models and create sims
 print("fit models")
-inv1 = inv.copy()
+_inv = inv.copy()
 mf = MatrixFactorization.load("../../models/ml-small-mf")
 mf.data = uir_data
-sim1 = RandomFromTopNSimulator("best score", uir_data, mf, inv1)
+sim1 = RandomFromTopNSimulator("best score", uir_data, mf, _inv)
 
-inv2 = inv.copy()
-wr1 = WeightedRecommender(inv2, mf, alpha=0.5)
-wr1.fit(uir_data)
-sim2 = RandomFromTopNSimulator(f"alpha={wr1.alpha}", uir_data, wr1, inv2)
+_inv = inv.copy()
+wr = WeightedRecommender(_inv, mf, alpha=0.9)
+wr.fit(uir_data)
+sim2 = RandomFromTopNSimulator(f"alpha={wr.alpha}", uir_data, wr, _inv)
 
-inv3 = inv.copy()
-wr2 = WeightedRecommender(inv3, mf, alpha=0.75)
-wr2.fit(uir_data)
-sim3 = RandomFromTopNSimulator(f"alpha={wr2.alpha}", uir_data, wr2, inv3)
+_inv = inv.copy()
+wr = WeightedRecommender(_inv, mf, alpha=0.85)
+wr.fit(uir_data)
+sim3 = RandomFromTopNSimulator(f"alpha={wr.alpha}", uir_data, wr, _inv)
 
-inv4 = inv.copy()
-wr3 = WeightedRecommender(inv4, mf, alpha=0.85)
-wr3.fit(uir_data)
-sim4 = RandomFromTopNSimulator(f"alpha={wr3.alpha}", uir_data, wr3, inv4)
+_inv = inv.copy()
+wr = WeightedRecommender(_inv, mf, alpha=0.8)
+wr.fit(uir_data)
+sim4 = RandomFromTopNSimulator(f"alpha={wr.alpha}", uir_data, wr, _inv)
 
-inv5 = inv.copy()
-miir = MostInInvRecommender(inv5)
+_inv = inv.copy()
+wr = WeightedRecommender(_inv, mf, alpha=0.75)
+wr.fit(uir_data)
+sim5 = RandomFromTopNSimulator(f"alpha={wr.alpha}", uir_data, wr, _inv)
+
+_inv = inv.copy()
+wr = WeightedRecommender(_inv, mf, alpha=0.7)
+wr.fit(uir_data)
+sim6 = RandomFromTopNSimulator(f"alpha={wr.alpha}", uir_data, wr, _inv)
+
+_inv = inv.copy()
+miir = MostInInvRecommender(_inv)
 miir.fit(uir_data)
-sim5 = RandomFromTopNSimulator("most in inv", uir_data, miir, inv5)
+sim7 = RandomFromTopNSimulator("most in inv", uir_data, miir, _inv)
 
 # run simulations
 print("run simulations")
 ms = MultiSimulator(50_000)
-ms.set_sims([sim1, sim2, sim3, sim4, sim5])
+ms.set_sims([sim1, sim2, sim3, sim4, sim5, sim6, sim7])
 ms.run_parallel()
 
 # plot data
 print("plot data")
-sim1.plot("../../figures/ml_inv_mf.png")
-sim2.plot("../../figures/ml_inv_alpha_50.png")
-sim3.plot("../../figures/ml_inv_alpha_75.png")
-sim4.plot("../../figures/ml_inv_alpha_85.png")
-sim5.plot("../../figures/ml_inv_mii.png")
 
-RandomFromTopNSimulator.multi_plot([sim1, sim2, sim3, sim4, sim5],
+figure_file = "../../figures/ml"
+if UNIFORM:
+    figure_file += "_uinv"
+else:
+    figure_file += "_inv"
+
+sim1.plot(figure_file + "_mf.png")
+sim2.plot(figure_file + "_alpha_90.png")
+sim3.plot(figure_file + "_alpha_85.png")
+sim4.plot(figure_file + "_alpha_80.png")
+sim5.plot(figure_file + "_alpha_75.png")
+sim6.plot(figure_file + "_alpha_70.png")
+sim7.plot(figure_file + "_mii.png")
+
+RandomFromTopNSimulator.multi_plot([sim1, sim2, sim3, sim4, sim5, sim6, sim7],
                                    data="sold_items",
-                                   save_file="../../figures/ml_inv_sold.png")
-RandomFromTopNSimulator.multi_plot([sim1, sim2, sim3, sim4, sim5],
+                                   save_file=figure_file + "_sold.png")
+RandomFromTopNSimulator.multi_plot([sim1, sim2, sim3, sim4, sim5, sim6, sim7],
                                    data="empty_items",
-                                   save_file="../../figures/ml_inv_empty.png")
+                                   save_file=figure_file + "_empty.png")
