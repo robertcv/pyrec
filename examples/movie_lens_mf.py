2f9cff9e10372a4233df8822ce75ea1a45b46828
robert <robert.cvitkovic@gmail.com>
2020-03-05 12:01:46 +0100

    update post recs

--- examples/movie_lens_mf.py
+++ examples/movie_lens_mf.py
@@ -1,49 +1,61 @@
 from pyrec.data import UIRData
 from pyrec.inventory import Inventory
-from pyrec.parallel import MultiSimulator
 from pyrec.sims.rand import RandomFromTopNSimulator
-from pyrec.plots import multi_plot, plot_ratings_violin, multi_success_stops
-from pyrec.recs.mf import MatrixFactorization
+from pyrec.sims.repeated import RepeatedSimulation
+from pyrec.parallel import MultiSimulator
+from pyrec.plots import multi_success_stops, multi_success_err
+from pyrec.recs.mf import MatrixFactorization, NNMatrixFactorization
 from pyrec.recs.post import UnbiasedMatrixFactorization, \
-    UnbiasedUsersMatrixFactorization, UserWantMatrixFactorization
+    UnbiasedUsersMatrixFactorization, UserWantMatrixFactorization, \
+    UserNotWantMatrixFactorization
 
 
 RATINGS_FILE = "../data/MovieLens/ml-latest-small/ratings.csv"
 uir_data = UIRData.from_csv(RATINGS_FILE)
+inv = Inventory(uir_data)
 figure_file = "../figures/ml_inv_mf"
 
+k = 30
+n = 20_000
+rec_kwargs = {"verbose": True, "max_iteration": 200, "batch_size": 100,
+              "rec": MatrixFactorization,
+              "rec_kwargs": {"max_iteration": 200, "batch_size": 100}}
+sim_kwargs = {"verbose": True}
+
+recs = [("mf", MatrixFactorization),
+        ("nnmf", NNMatrixFactorization),
+        ("umf", UnbiasedMatrixFactorization),
+        ("uumf", UnbiasedUsersMatrixFactorization),
+        ("uwmf", UserWantMatrixFactorization),
+        ("unwmf", UserNotWantMatrixFactorization)]
+
+# sims = []
+# for name, rec in recs:
+#     inv = Inventory(uir_data)
+#     r = rec(**rec_kwargs)
+#     r.fit(uir_data)
+#     sims.append(RandomFromTopNSimulator(name, uir_data, r, inv))
+#
+# # run simulations
+# print("run simulations")
+# ms = MultiSimulator(n)
+# ms.set_sims(sims)
+# ms.run_parallel()
+#
+# multi_success_stops(sims, list(range(1_000, n + 1, 500)),
+#                     save_file=figure_file + "_20k_success_step.png")
+
+# repeated
+
+rec_kwargs["verbose"] = False
+sim_kwargs["verbose"] = False
+
 sims = []
-inv = Inventory(uir_data)
-mf = MatrixFactorization(max_iteration=200, batch_size=100)
-mf.fit(uir_data)
-sims.append(RandomFromTopNSimulator(f"mf", uir_data, mf, inv))
+for name, rec in recs:
+    rs = RepeatedSimulation(name, uir_data, inv,
+                            rec, rec_kwargs,
+                            RandomFromTopNSimulator, sim_kwargs)
+    rs.run(n, k)
+    sims.append(rs)
 
-inv = Inventory(uir_data)
-umf = UnbiasedMatrixFactorization(max_iteration=200, batch_size=100)
-umf.fit(uir_data)
-sims.append(RandomFromTopNSimulator(f"umf", uir_data, umf, inv))
-
-inv = Inventory(uir_data)
-uumf = UnbiasedUsersMatrixFactorization(max_iteration=200, batch_size=100)
-uumf.fit(uir_data)
-sims.append(RandomFromTopNSimulator(f"uumf", uir_data, uumf, inv))
-
-inv = Inventory(uir_data)
-uwmf = UserWantMatrixFactorization(max_iteration=200, batch_size=100)
-uwmf.fit(uir_data)
-sims.append(RandomFromTopNSimulator(f"uwmf", uir_data, uwmf, inv))
-
-
-ms = MultiSimulator(20_000)
-ms.set_sims(sims)
-ms.run_parallel()
-
-
-plot_ratings_violin(sims[0], save_file=figure_file + "_rviola_mf.png")
-plot_ratings_violin(sims[0], save_file=figure_file + "_rviola_umf.png")
-plot_ratings_violin(sims[0], save_file=figure_file + "_rviola_uumf.png")
-plot_ratings_violin(sims[0], save_file=figure_file + "_rviola_uwmf.png")
-
-multi_plot(sims, data="sold_i", save_file=figure_file + "_sold.png")
-multi_success_stops(sims, list(range(1_000, 20_001, 1_000)),
-                    save_file=figure_file + "_20k_success_step.png")
+multi_success_err(sims, save_file=figure_file + "_20k_success_err.png")
--- pyrec/recs/post.py
+++ pyrec/recs/post.py
@@ -1,67 +1,104 @@
 import numpy as np
 
+from pyrec.recs.base import BaseRecommender
 from pyrec.recs.mf import MatrixFactorization
 
 
-class UnbiasedMatrixFactorization(MatrixFactorization):
+class BasePost(BaseRecommender):
+    def __init__(self, rec, rec_kwargs, verbose=True, **kwargs):
+        self.verbose = verbose
+
+        self.rec = rec  # type: BaseRecommender
+        self.rec_kwargs = rec_kwargs
+        self.rec_kwargs["verbose"] = self.verbose
+
+        self.rec_fitted = True
+
+        if not isinstance(self.rec, MatrixFactorization):
+            self.rec_fitted = False
+            self.rec = self.rec(**self.rec_kwargs)
+
+        super().__init__()
+
+    def fit(self, data):
+        super().fit(data)
+        if not self.rec_fitted:
+            self.rec.fit(data)
+
+    def __setattr__(self, name, value):
+        if name == "data":
+            self.rec.data = value
+        self.__dict__[name] = value
+
+    def save(self, file_name):
+        self.rec.save(file_name + "_1")
+
+    def load(self, file_name):
+        self.rec.load(file_name + "_1")
+
+
+class UnbiasedMatrixFactorization(BasePost):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        assert isinstance(self.rec, MatrixFactorization)
         self.p_max = 0
         self.p_min = 0
         self.t_max = 0
         self.t_min = 0
 
-    def fit(self, data):
-        super().fit(data)
-        full = np.dot(self.P, self.Q.T)
-        self.p_max = np.max(full)
-        self.p_min = np.min(full)
+    def _pre_compute(self):
+        full = np.dot(self.rec.P, self.rec.Q.T)
+        self.p_max = full.max()
+        self.p_min = full.min()
         self.t_min = self.data.train_data.ratings.min()
         self.t_max = self.data.train_data.ratings.max()
 
     def _trans(self, x):
+        if self.p_max == 0:
+            self._pre_compute()
         norm_x = (x - self.p_min) / (self.p_max - self.p_min)
         return norm_x * (self.t_max - self.t_min) + self.t_min
 
     def _predict(self, user_index, item_index):
-        return self._trans(np.sum(self.P[user_index, :] * self.Q[item_index, :]))
+        return self._trans(
+            np.sum(self.rec.P[user_index, :] * self.rec.Q[item_index, :]))
 
     def _predict_user(self, user_index):
         return self._trans(
-            np.sum(self.P[np.repeat(user_index, self.data.m), :] *
-                   self.Q[np.arange(self.data.m), :], axis=1))
+            np.sum(self.rec.P[np.repeat(user_index, self.data.m), :] *
+                   self.rec.Q[np.arange(self.data.m), :], axis=1))
 
 
 class UnbiasedUsersMatrixFactorization(UnbiasedMatrixFactorization):
-    def fit(self, data):
-        super().fit(data)
-        full = np.dot(self.P, self.Q.T) + self.i_bayes
-        self.p_max = np.max(full)
-        self.p_min = np.min(full)
+    def _pre_compute(self):
+        full = np.dot(self.rec.P, self.rec.Q.T) + self.rec.i_bayes
+        self.p_max = full.max()
+        self.p_min = full.min()
         self.t_min = self.data.train_data.ratings.min()
         self.t_max = self.data.train_data.ratings.max()
 
     def _predict(self, user_index, item_index):
-        return self._trans(np.sum(self.P[user_index, :] *
-                                  self.Q[item_index, :]) +
-                           self.i_bayes[item_index])
+        return self._trans(np.sum(self.rec.P[user_index, :] *
+                                  self.rec.Q[item_index, :]) +
+                           self.rec.i_bayes[item_index])
 
     def _predict_user(self, user_index):
         return self._trans(
-            np.sum(self.P[np.repeat(user_index, self.data.m), :] *
-                   self.Q[np.arange(self.data.m), :], axis=1) + self.i_bayes)
+            np.sum(self.rec.P[np.repeat(user_index, self.data.m), :] *
+                   self.rec.Q[np.arange(self.data.m), :], axis=1) +
+            self.rec.i_bayes)
 
 
-class UserWantMatrixFactorization(MatrixFactorization):
+class UserNotWantMatrixFactorization(BasePost):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        assert isinstance(self.rec, MatrixFactorization)
         self.full = None
         self.t_max = 0
         self.t_min = 0
 
-    def fit(self, data):
-        super().fit(data)
-        self.full = np.dot(self.P, self.Q.T) + self.i_bayes
+    def _pre_compute(self):
+        self.full = np.dot(self.rec.P, self.rec.Q.T)
         self.t_min = self.data.train_data.ratings.min()
         self.t_max = self.data.train_data.ratings.max()
 
@@ -69,16 +106,42 @@ class UserWantMatrixFactorization(MatrixFactorization):
         return x * (self.t_max - self.t_min) + self.t_min
 
     def _predict(self, user_index, item_index):
+        if self.full is None:
+            self._pre_compute()
         user_r = self.full[user_index, item_index]
         want = np.sum(self.full[:, item_index] > user_r)
         not_want = 1 - want / self.data.n
         return self._trans(not_want)
 
     def _predict_user(self, user_index):
+        if self.full is None:
+            self._pre_compute()
         user_r = self.full[user_index, :]
         want = np.sum(self.full > user_r, axis=0)
         not_want = 1 - want / self.data.n
         return self._trans(not_want)
+
+
+class UserWantMatrixFactorization(UserNotWantMatrixFactorization):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.full = None
+        self.t_max = 0
+        self.t_min = 0
+
+    def _predict(self, user_index, item_index):
+        if self.full is None:
+            self._pre_compute()
+        user_r = self.full[user_index, item_index]
+        want = np.sum(self.full[:, item_index] > user_r)
+        return self._trans(want / self.data.n)
+
+    def _predict_user(self, user_index):
+        if self.full is None:
+            self._pre_compute()
+        user_r = self.full[user_index, :]
+        want = np.sum(self.full > user_r, axis=0)
+        return self._trans(want / self.data.n)
 
 
 if __name__ == '__main__':
@@ -86,7 +149,7 @@ if __name__ == '__main__':
     RATINGS_FILE = "../../data/MovieLens/ml-latest-small/ratings.csv"
     uir_data = UIRData.from_csv(RATINGS_FILE)
 
-    mf = UserWantMatrixFactorization(k=20, max_iteration=20,
+    mf = UnbiasedMatrixFactorization(k=20, max_iteration=200,
                                      batch_size=100)
     mf.fit(uir_data)
     mf.predict(uir_data.unique_values.users[0],
