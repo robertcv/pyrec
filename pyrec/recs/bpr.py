73eed6b257efc44e3764a5b2d098db10a49ff2eb
Unknown <robert.cvitkovic@gmail.com>
2020-07-13 14:44:59 +0200

    some minor improvements to bpr

--- pyrec/recs/bpr.py
+++ pyrec/recs/bpr.py
@@ -19,16 +19,14 @@ class BPR(MatrixFactorization):
         np.random.seed(self.seed)
         self._init_matrix()
 
-        train_i = data.train_data.items
-        unique_t_u, user_t_r = self._preprocess(data.train_data)
-        validation_i = data.validation_data.items
-        unique_v_u, user_v_r = self._preprocess(data.validation_data)
+        train_users, train_user_item = self._preprocess(data.train_data)
+        validation_users, validation_user_item = self._preprocess(data.validation_data)
 
         def _validation_loss():
             bpr_opt = []
-            for u in unique_v_u:
-                i = validation_i[user_v_r[u]['pos']]
-                j = validation_i[user_v_r[u]['neg']]
+            for u in validation_users:
+                i = validation_user_item[u]['pos']
+                j = np.random.choice(validation_user_item[u]['neg'], size=len(i))
                 x_ui = np.sum(self.P[u, :] * self.Q[i, :]) + self.i_bayes[i]
                 x_uj = np.sum(self.P[u, :] * self.Q[j, :]) + self.i_bayes[j]
                 x_uij = x_ui[:, None] - x_uj
@@ -40,9 +38,9 @@ class BPR(MatrixFactorization):
 
         for iteration in range(self.max_iteration):
             start_t = time.time()
-            u = np.random.choice(unique_t_u, size=self.batch_size)
-            i = np.array([train_i[np.random.choice(user_t_r[_u]['pos'])] for _u in u])
-            j = np.array([train_i[np.random.choice(user_t_r[_u]['neg'])] for _u in u])
+            u = np.random.choice(train_users, size=self.batch_size)
+            i = np.array([np.random.choice(train_user_item[_u]['pos']) for _u in u])
+            j = np.array([np.random.choice(train_user_item[_u]['neg']) for _u in u])
             self._update(u, i, j)
 
             if iteration % 50 == 0 and iteration != 0:
@@ -83,7 +81,7 @@ class BPR(MatrixFactorization):
             pos = np.where(u_bool & (data.ratings > r_mean))[0]
             if pos.size < 1:
                 continue
-            processed_data[u] = {'pos': pos, 'neg': neg}
+            processed_data[u] = {'pos': data.items[pos], 'neg': data.items[neg]}
         unique_u = np.array(list(processed_data.keys()))
         return unique_u, processed_data
 
@@ -103,10 +101,11 @@ class UnbiasedBPR(BPR):
 
 
 if __name__ == '__main__':
-    RATINGS_FILE = "../../data/MovieLens/ml-latest-small/ratings.csv"
-    uir_data = UIRData.from_csv(RATINGS_FILE)
+    # RATINGS_FILE = "../../data/MovieLens/ml-latest-small/ratings.csv"
+    RATINGS_FILE = "../../data/retail/nakupi.csv"
+    uir_data = UIRData.from_implicit_csv(RATINGS_FILE)
 
-    bpr = BPR(k=20, alpha=0.01, mi=0.001, batch_size=500)
+    bpr = BPR(k=20, alpha=0.01, mi=0.001, batch_size=500, implicit=True)
     bpr.fit(uir_data)
 
     r = bpr.predict(uir_data.unique_values.users[0],
