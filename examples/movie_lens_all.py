e4f28f5c53890ce8a25007fcde7c64ee9069fc59
robert <robert.cvitkovic@gmail.com>
2020-07-21 16:44:32 +0200

    update test all

--- examples/movie_lens_all.py
+++ examples/movie_lens_all.py
@@ -1,54 +1,69 @@
 from pyrec.data import UIRData
 from pyrec.inventory import Inventory
-from pyrec.recs.mf import MatrixFactorization
-from pyrec.recs.inv import MostInInvStaticRecommender, MostInInvRecommender
-from pyrec.recs.post import UnbiasedMatrixFactorization
-from pyrec.simulator import RandomFromTopNSimulator, multi_success_stops
+from pyrec.sims.rand import BestSimulator
 from pyrec.parallel import MultiSimulator
+from pyrec.plots import multi_success_stops, multi_success_err, plot_ratings_dist
+from pyrec.recs.mf import MatrixFactorization, NNMatrixFactorization, \
+    RandomMatrixFactorization, UnbiasedMatrixFactorization
+from pyrec.recs.cap_mf import CapMF
+from pyrec.recs.inv import MostInInvStaticRecommender, MostInInvRecommender
+from pyrec.recs.weighted import WeightedRecommender
 
 
 RATINGS_FILE = "../data/MovieLens/ml-latest-small/ratings.csv"
+uir_data = UIRData.from_csv(RATINGS_FILE)
+inv = Inventory(uir_data)
+figure_file = "../figures/inv"
 
-# fit models and create sims
-print("fit models")
+k = 30
+n = 20_000
+rec_kwargs = {"verbose": True, "max_iteration": 200, "batch_size": 100,
+              "rec": UnbiasedMatrixFactorization,
+              "rec_kwargs": {"max_iteration": 200, "batch_size": 100},
+              "walpha": 0.9, "rec1": UnbiasedMatrixFactorization,
+              "rec1_kwargs": {"max_iteration": 200, "batch_size": 100},
+              "rec2": MostInInvStaticRecommender, "rec2_kwargs": {}}
+sim_kwargs = {"verbose": True}
+
+recs = [
+    ("umf", UnbiasedMatrixFactorization),
+    ("miis", MostInInvStaticRecommender),
+    ("r", RandomMatrixFactorization),
+    ("wrs", WeightedRecommender),
+    ("cap", CapMF),
+]
+
 sims = []
-
-uir_data = UIRData.from_csv(RATINGS_FILE)
-inv = Inventory(uir_data)
-mf = MatrixFactorization(max_iteration=200, batch_size=100)
-mf.fit(uir_data)
-sims.append(RandomFromTopNSimulator(f"mf", uir_data, mf, inv))
-
-uir_data = UIRData.from_csv(RATINGS_FILE)
-inv = Inventory(uir_data)
-umf = UnbiasedMatrixFactorization(max_iteration=200, batch_size=100)
-umf.fit(uir_data)
-sims.append(RandomFromTopNSimulator(f"umf", uir_data, umf, inv))
-
-uir_data = UIRData.from_csv(RATINGS_FILE)
-inv = Inventory(uir_data)
-miir = MostInInvRecommender(inv)
-miir.fit(uir_data)
-sims.append(RandomFromTopNSimulator(f"mii", uir_data, miir, inv))
-
-uir_data = UIRData.from_csv(RATINGS_FILE)
-inv = Inventory(uir_data)
-miisr = MostInInvStaticRecommender(inv)
-miisr.fit(uir_data)
-sims.append(RandomFromTopNSimulator(f"miis", uir_data, miisr, inv))
-
+for name, rec in recs:
+    inv = Inventory(uir_data)
+    rec_kwargs["inv"] = inv
+    rec_kwargs["rec2_kwargs"]["inv"] = inv
+    r = rec(**rec_kwargs)
+    r.fit(uir_data)
+    sims.append(BestSimulator(name, uir_data, r, inv))
 
 # run simulations
 print("run simulations")
-ms = MultiSimulator(20_000)
+ms = MultiSimulator(n)
 ms.set_sims(sims)
 ms.run_parallel()
 
-# plot data
-print("plot data")
+multi_success_stops(sims, list(range(1_000, n + 1, 500)),
+                    save_file=figure_file + "_best_20k_success_step.png")
 
-figure_file = "../figures/ml"
-figure_file += "_inv"
+# repeated
 
-multi_success_stops(sims, list(range(1_000, 20_001, 500)),
-                    save_file=figure_file + "_20k_success_step.png")
+# rec_kwargs["rec2_kwargs"]["inv"] = inv
+# rec_kwargs["inv"] = inv
+# rec_kwargs["verbose"] = False
+# sim_kwargs["verbose"] = False
+#
+# sims = []
+# for name, rec in recs:
+#     rs = RepeatedSimulation(name, uir_data, inv,
+#                             rec, rec_kwargs,
+#                             BestSimulator, sim_kwargs)
+#     rs.run(n, k)
+#     sims.append(rs)
+#
+# multi_success_err(sims, save_file=figure_file + "_best_20k_success_err.png")
