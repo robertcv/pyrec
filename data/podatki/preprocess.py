3ccaa08d067336bb1b676cc3247a8601dac5d570
Unknown <robert.cvitkovic@gmail.com>
2020-08-24 17:12:11 +0200

    update  data preprocessing

--- data/podatki/preprocess.py
+++ data/podatki/preprocess.py
@@ -1,44 +1,58 @@
-import csv
 import pandas as pd
 
+pd.set_option('display.max_columns', 10)
+pd.set_option('display.width', 1000)
 
 df = pd.read_csv("nakupi.csv", parse_dates=[2])
+print('opened file')
 
-item_user_count = df.groupby(['gn', 'artikel'], as_index=False).sum()[['gn', 'artikel', 'kolicina']]
-item_min_count = item_user_count.groupby(['artikel']).min()
-item_min_count[item_min_count.kolicina < 0] = 0
-item_max_count = item_user_count.groupby(['artikel']).max()
+# for each user, item, document sum up all item counts
+user_item_doc_sum = df.groupby(['gn', 'artikel', 'dokument']).sum()[['kolicina']]
+user_item_doc_sum.kolicina[user_item_doc_sum.kolicina < 0] = 0
+# get average item count over all documents for each user
+user_item = user_item_doc_sum.groupby(['gn', 'artikel']).mean()[['kolicina']]
 
-item_user_time = df.groupby(['gn', 'artikel'], as_index=False).max()[['gn', 'artikel', 'datum_prometa']]
-item_min_time = df.groupby(['artikel']).min()
-item_max_time = df.groupby(['artikel']).max()
+# get min and max item count over all documents
+item_doc_sum = df.groupby(['artikel', 'dokument'], as_index=False).sum()[['artikel', 'dokument', 'kolicina']]
+item_doc_sum.kolicina[item_doc_sum.kolicina < 0] = 0
+item_doc_min_count = item_doc_sum.groupby(['artikel']).min()[['kolicina']]
+item_doc_max_count = item_doc_sum.groupby(['artikel']).max()[['kolicina']]
 
+# calculate a score of how much of an item users bay compered to other users
+user_item = user_item.join(item_doc_min_count, lsuffix='', rsuffix='_all_min')
+user_item = user_item.join(item_doc_max_count, lsuffix='', rsuffix='_all_max')
+user_item['item_count_score'] = (user_item['kolicina'] - user_item['kolicina_all_min']) / \
+                                (user_item['kolicina_all_max'] - user_item['kolicina_all_min'])
+user_item['item_count_score'] = user_item['item_count_score'].fillna(0)
+print('score 1')
 
-def time2rating(item, time_obj):
-    min_time = item_min_time.loc[item, 'datum_prometa']
-    max_time = item_max_time.loc[item, 'datum_prometa']
-    if (max_time - min_time) == pd.Timedelta(0):
-        return 1
-    return (time_obj - min_time) / (max_time - min_time)
+# get number of documents different documents for each user
+user_doc_count = df.groupby(['gn']).agg({"dokument": "nunique"})
+# get in how many different documents an item is bought
+user_item_doc_count = df.groupby(['gn', 'artikel']).agg({"dokument": "nunique"})
 
+# calculate a score on in how many different documents are items contained
+user_item = user_item.join(user_doc_count)
+user_item = user_item.join(user_item_doc_count, lsuffix='', rsuffix='_item')
+user_item['item_doc_score'] = user_item['dokument_item'] / user_item['dokument']
+print('score 2')
 
-def count2rating(item, count):
-    min_count = item_min_count.loc[item, 'kolicina']
-    max_count = item_max_count.loc[item, 'kolicina']
-    if (max_count - min_count) == 0:
-        return 1
-    return (count - min_count) / (max_count - min_count)
+# get latest purchasing date for item, user pair
+item_user_latest = df.groupby(['gn', 'artikel']).max()[['datum_prometa']]
+# get min and max date of items
+item_min_time = df.groupby(['artikel']).min()[['datum_prometa']]
+item_max_time = df.groupby(['artikel']).max()[['datum_prometa']]
 
+# calculate a score of when was the last time an item was purchased
+user_item = user_item.join(item_user_latest)
+user_item = user_item.join(item_min_time, lsuffix='', rsuffix='_min')
+user_item = user_item.join(item_max_time, lsuffix='', rsuffix='_max')
+user_item['item_time_score'] = (user_item['datum_prometa'] - user_item['datum_prometa_min']) / \
+                                (user_item['datum_prometa_max'] - user_item['datum_prometa_min'])
+user_item['item_time_score'] = user_item['item_time_score'].fillna(0)
+print('score 3')
 
-res = [['gn', 'artikel', 'rating']]
-for c, t in zip(item_user_count.iterrows(), item_user_time.iterrows()):
-    assert c[1]['gn'] == t[1]['gn'] and c[1]['artikel'] == t[1]['artikel']
-    item = c[1]['artikel']
-    t_rating = time2rating(item, t[1]['datum_prometa'])
-    c_rating = count2rating(item, c[1]['kolicina'])
-    res.append([int(c[1]['gn']), int(c[1]['artikel']), t_rating + c_rating])
-
-
-with open("ratings.csv", "w") as f:
-    writer = csv.writer(f)
-    writer.writerows(res)
+# sum up scores and save to file
+user_item['score'] = user_item['item_count_score'] + user_item['item_doc_score'] + user_item['item_time_score']
+user_item = user_item.reset_index()[['gn', 'artikel', 'score']]
+user_item.to_csv('ratings.csv', index=False)
