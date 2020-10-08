48e9e9c96a7b28c73bd0fc204cbc26ad11a17e4d
Unknown <robert.cvitkovic@gmail.com>
2020-10-08 21:00:16 +0200

    update examples

--- examples/plot_sims_w.py
+++ examples/plot_sims_w.py
@@ -19,54 +19,56 @@ figure_file = "../figures/podatki"
 
 k = 30
 n = 200_000
-rec_kwargs = {"verbose": True, "max_iteration": 200, "batch_size": 1000,
+rec_kwargs = {"verbose": True, "max_iteration": 200, "batch_size": 100,
               "rec": MatrixFactorization,
-              "rec_kwargs": {"max_iteration": 200, "batch_size": 1000},
+              "rec_kwargs": {"max_iteration": 200, "batch_size": 100},
               "walpha": 0.7, "rec1": MatrixFactorization,
-              "rec1_kwargs": {"max_iteration": 200, "batch_size": 1000},
+              "rec1_kwargs": {"max_iteration": 200, "batch_size": 100},
               "rec2": MostInInvRecommender, "rec2_kwargs": {}}
 sim_kwargs = {"verbose": True}
 
 recs = [
-    ("rand", RandomMatrixFactorization),
-    ("mf", MatrixFactorization),
-    ("nnmf", NNMatrixFactorization),
-    ("miis", MostInInvStaticRecommender),
-    ("mii", MostInInvRecommender),
-    ("wrs", WeightedRecommender),
+    ("wrs a=0", WeightedRecommender, 0),
+    ("wrs a=0.2", WeightedRecommender, 0.2),
+    ("wrs a=0.4", WeightedRecommender, 0.4),
+    ("wrs a=0.6", WeightedRecommender, 0.6),
+    ("wrs a=0.8", WeightedRecommender, 0.8),
+    ("wrs a=1", WeightedRecommender, 1),
 ]
 
 sims = []
-for name, rec in recs:
+for name, rec, a in recs:
     inv = Inventory(uir_data)
     rec_kwargs["inv"] = inv
+    rec_kwargs["walpha"] = a
     rec_kwargs["rec2_kwargs"]["inv"] = inv
     r = rec(**rec_kwargs)
     r.fit(uir_data)
     sims.append(BestSimulator(name, uir_data, r, inv))
 
 # run simulations
-# print("run simulations")
-# ms = MultiSimulator(n)
-# ms.set_sims(sims)
-# ms.run_parallel()
-#
-# multi_success_stops(sims, list(range(1_000, n + 1, 1_000)),
-#                     save_file=figure_file + "_best_200k_success_step.png")
+print("run simulations")
+ms = MultiSimulator(n)
+ms.set_sims(sims)
+ms.run_parallel()
+
+multi_success_stops(sims, list(range(1_000, n + 1, 1_000)),
+                    save_file=figure_file + "_best_w_200k_success_step.png")
 
 # repeated
 
-rec_kwargs["rec2_kwargs"]["inv"] = inv
-rec_kwargs["inv"] = inv
-rec_kwargs["verbose"] = False
-sim_kwargs["verbose"] = False
-
-sims = []
-for name, rec in recs:
-    rs = RepeatedSimulation(name, uir_data, inv,
-                            rec, rec_kwargs,
-                            BestSimulator, sim_kwargs)
-    rs.run(n, k)
-    sims.append(rs)
-
-multi_success_err(sims, save_file=figure_file + "_best_200k_success_err.png")
+# rec_kwargs["rec2_kwargs"]["inv"] = inv
+# rec_kwargs["inv"] = inv
+# rec_kwargs["verbose"] = False
+# sim_kwargs["verbose"] = False
+#
+# sims = []
+# for name, rec, a in recs:
+#     rec_kwargs["walpha"] = a
+#     rs = RepeatedSimulation(name, uir_data, inv,
+#                             rec, rec_kwargs,
+#                             BestSimulator, sim_kwargs)
+#     rs.run(n, k)
+#     sims.append(rs)
+#
+# multi_success_err(sims, save_file=figure_file + "_best_w_200k_success_err.png")
